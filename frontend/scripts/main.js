5573da26f397232aac3d75e10e4af0d5b9c3973f
RicardoMC310 <ricardo.matos72.etec@gmail.com>
2025-09-10 10:27:32 -0300

    sistema de menu melhorado (frontend)

--- frontend/scripts/main.js
+++ frontend/scripts/main.js
@@ -1,16 +1,18 @@
 const ul = document.querySelector("#header-container #links-tabs ul");
 const menuButton = document.querySelector("#header-container #menu-container #menu-button");
 
+ul.style.display = "none";
+
 menuButton.addEventListener("click", () => {
     if (ul.style.display == "none") {
         ul.style.display = "inline-flex";
         ul.classList.add("menu-tabs-links-show");
         ul.classList.remove("menu-tabs-links-hidden");
-        menuButton.classList.add("menu-button-active");
+        //menuButton.classList.add("menu-button-active");
     }else {
         ul.classList.remove("menu-tabs-links-show");
         ul.classList.add("menu-tabs-links-hidden");
         setTimeout(() => ul.style.display = "none", 250);
-        menuButton.classList.remove("menu-button-active");
+        //menuButton.classList.remove("menu-button-active");
     }
 });
